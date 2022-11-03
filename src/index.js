976e89622f9aeb64aa1b8ffba3621e5ae6ced7ec
Anna Yagofarova <nuta.max4@gmail.com>
2022-11-03 23:46:58 +0300

    new file with general logics

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import readlineSync from 'readline-sync';
+
+const rounds = 3;
+
+const startGame = (gameVariant, rules) => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log(rules);
+
+  for (let i = 0; i < rounds; i += 1) {
+    const [question, correctAnswer] = gameVariant();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer !== correctAnswer) {
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
+      );
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+
+export default startGame;
