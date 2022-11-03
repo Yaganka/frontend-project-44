3001c4bbb7f47fdd727db7a3ddce6a5cf496ce79
Anna Yagofarova <nuta.max4@gmail.com>
2022-11-03 23:51:21 +0300

    restored the previous version

--- src/cli.js
+++ src/cli.js
@@ -3,8 +3,6 @@ import readlineSync from 'readline-sync';
 const helloDialog = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  return name;
 };
-const userName = helloDialog();
 
-export { helloDialog, userName };
+export default helloDialog;
