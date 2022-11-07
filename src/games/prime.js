1333435147baa771497989aa1ee214c50ed7bca2
Anna Yagofarova <nuta.max4@gmail.com>
2022-11-07 20:15:59 +0300

    no errors

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,19 +4,19 @@ import getRandom from '../getRandom.js';
 const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 function isPrime(randomNum) {
-    for (let i = 2, max = Math.sqrt(randomNum); i <= max; i += 1) {
-        if (randomNum % i === 0) {
-            return false;
-        }
+  for (let i = 2, max = Math.sqrt(randomNum); i <= max; i += 1) {
+    if (randomNum % i === 0) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 const brainPrime = () => {
-    const randomNum = getRandom();
-    const question = randomNum;
-    const correctAnswer = isPrime(question) ? 'yes' : 'no';
-    return [question, correctAnswer];
+  const randomNum = getRandom();
+  const question = randomNum;
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
 };
 
 export default () => startGame(brainPrime, rules);
